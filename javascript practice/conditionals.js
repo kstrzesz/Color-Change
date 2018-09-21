b1307a01807b6f4e52739e94dac1ad7a98aa595b
kstrzesz <kdeezy@Kristas-MacBook-Pro.local>
2018-09-21 09:05:50 -0400

    added practive js

---
+++ javascript practice/conditionals.js
@@ -0,0 +1,12 @@
+let outcome;
+if (15 === 14) {
+    outcome = "okay"
+} else if ("dog" == "cat") {
+    outcome = "fine"
+} else if ("krista" === "kristsa") {
+    outcome = "wow!"
+} else {
+    outcome = "thats it"
+}
+
+console.log(outcome)
---
+++ javascript practice/functions.js
@@ -0,0 +1,34 @@
+// This is where we define our functions
+function addTwoNumbers(x, y) {
+    return x + y;
+}
+
+function greeting() {
+    return 'Hello World!'
+}
+
+function square(x) { 
+    return x * x
+}
+
+// This is where we will test our functions
+
+// this is a single line comment is JS
+/*
+    this is a mutli-line comment in JS
+*/
+// console.log('this is a console.log') // will print output in console in html site
+// alert('this is an alert') // will print output upon webpage being opened
+
+console.log(addTwoNumbers(4, 10))
+
+console.log(greeting())
+console.log(square(9))
+
+let chase = "hi there 'come to door'"
+
+//to open up using the silent command go to terminal
+//cd .. 
+//cd Documents
+//cd Plural (tab to finish) 
+//enter code . 
