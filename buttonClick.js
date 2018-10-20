22f2e16c1150a69a3e66e9b75defdee776f5fce0
Krista Strzeszkowski <krista.cathleen.722@gmail.com>
2018-10-20 19:33:48 -0400

    no console.log

--- buttonClick.js
+++ buttonClick.js
@@ -2,9 +2,14 @@ function buttonclick() {
     let first = document.getElementById("fn").value
     let last = document.getElementById("ln").value
     console.log(first, last)
+    let validname = first + " " + last
+    console.log(validname)
+
     if (first.length > 0 && last.length > 0) {
         document.body.bgColor = randomcolor()
         console.log("hello")
+        
     }
+    
 }
     //change background color to a random color && is and
